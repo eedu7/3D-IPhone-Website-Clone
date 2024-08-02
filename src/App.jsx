5208ce46babf242572b10d0a75361bb1a35e5bba
eedu7 <mueedahmad067@gmail.com>
2024-08-02 16:21:09 +0500

    FEAT: Project Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import './App.css'
-import {Navbar} from "./components/Navbar.jsx";
-import {Hero} from "./components/Hero.jsx";
-import {HighLights} from "./components/HighLights.jsx";
-import {Model} from "./components/Model.jsx";
 
-function App() {
 
-  return (
-      <main className="bg-black">
-        <Navbar />
-        <Hero />
-        <HighLights />
-          <Model />
-      </main>
-  )
+import * as Sentry from '@sentry/react';
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import Highlights from "./components/HighLights.jsx";
+import Model from "./components/Model.jsx";
+import Features from "./components/Features.jsx";
+import HowItWorks from "./components/HowItWorks.jsx";
+import Footer from "./components/Footer.jsx";
+
+const App = () => {
+    return (
+        <main className="bg-black">
+            <Navbar />
+            <Hero />
+            <Highlights />
+            <Model />
+            <Features />
+            <HowItWorks />
+            <Footer />
+        </main>
+    )
 }
 
-export default App;
-
+export default Sentry.withProfiler(App);
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM, {createRoot} from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import * as Sentry from "@sentry/react";
+import app from "./App.jsx";
+
+Sentry.init({
+    dsn: "https://f97bdff87ba494aaa1ed3a282c0b17fc@o4507707461271552.ingest.de.sentry.io/4507707465531472",
+    integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.replayIntegration(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
 )
